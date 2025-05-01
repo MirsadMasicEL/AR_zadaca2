17703dfc1c729946610c9c432f75da0c3e33717b
MirsadMasicEL <mirsad.mail.prog@gmail.com>
2025-05-01 02:18:08 +0200

    k-ti commit

---
+++ zadatak4/main.c
@@ -0,0 +1,17 @@
+// Mirsad Masic, zadatak4
+#include <stdio.h>
+
+float bar(float, float, double);
+
+int main() {
+  float num = bar(1.5f, 2.6f, 3.0);
+  // ocekivano 4
+  printf("Rezultat f-je je: %f\n", num);
+  num = bar(1.7f, 1.7f, 5.8);
+  // ocekivano 6
+  printf("Rezultat f-je je: %f\n", num);
+  num = bar(2.3f, 1.1f, 82.7);
+  // ocekivano 82.7
+  printf("Rezultat f-je je: %f\n", num);
+  return 0;
+}
